ad8f39348c0d443c0dcbb60b18b44d5267dbb92b
common-selfeduc-git <118363851+common-selfeduc-git@users.noreply.github.com>
2023-01-27 02:39:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using static test228.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace test228
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string switchKey = "start";
+            while (switchKey != "exit")
+            {
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+                PrintMenu();
+                switchKey = Console.ReadLine();
+                switch (switchKey)
+                {
+                    case "Triangle":
+                        {
+                            Console.WriteLine("Введите длины сторон треугольника:");
+                            List<string> sides = new List<string>(3);
+                            for (int i = 0; i < 3; i++)
+                            {
+                                Console.Write($"{i + 1}: ");
+                                sides.Add(Console.ReadLine());
+                            }
+                            string side = "4";
+                            while ((int.Parse(side) != 1) && (int.Parse(side) != 2) && (int.Parse(side) != 3))
+                            {
+                                Console.Write("Введите номер стороны, к которой будет опущена высота!");
+                                side = Console.ReadLine();
+                            }
+                            Validator validator = new Validator();
+                            if (validator.typeError(sides) || validator.valueError(sides))
+                            {
+                                if (validator.valueError(sides))
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина стороны не может быть отрицательной!");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина должна выражаться числом!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Triangle triangle = new Triangle(double.Parse(sides[0]), double.Parse(sides[1]), double.Parse(sides[2]), Convert.ToInt32(side));
+                                if (triangle.PrivateTriangleValidation())
+                                {
+                                    triangle.ShowInfo();
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректный ввод длин сторон!");
+                                    break;
+                                }
+                            }
+                        }
+                    case "Circle":
+                        {
+                            Console.Write("Введите радиус окружности:");
+                            List<string> radius = new List<string>();
+                            radius.Add(Console.ReadLine());
+                            Validator validator = new Validator();
+                            if (validator.typeError(radius) || validator.valueError(radius))
+                            {
+                                if (validator.valueError(radius))
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина радиуса не может быть отрицательной!");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина должна выражаться числом!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Circle circle = new Circle(Convert.ToDouble(radius[0]));
+                                circle.ShowInfo();
+                                break;
+                            }
+                        }
+                    case "Square":
+                        {
+                            Console.Write("Введите сторону квадрата:");
+                            string length = Console.ReadLine();
+                            List<string> a = new List<string>() { length };
+                            Validator validator = new Validator();
+                            if (validator.typeError(a) || validator.valueError(a))
+                            {
+                                if (validator.valueError(a))
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина стороны не может быть отрицательной!");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина должна выражаться числом!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Square square = new Square(Convert.ToDouble(a[0]));
+                                square.ShowInfo();
+                                break;
+                            }
+                        }
+                    case "Rectangle":
+                        {
+                            Console.WriteLine("Введите стороны прямоугольника:");
+                            Console.Write("a: ");
+                            string length1 = Console.ReadLine();
+                            Console.Write("b: ");
+                            string length2 = Console.ReadLine();
+                            List<string> a = new List<string>() { length1, length2 };
+                            Validator validator = new Validator();
+                            if (validator.typeError(a) || validator.valueError(a))
+                            {
+                                if (validator.valueError(a))
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина радиуса не может быть отрицательной!");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Допущена ошибка при вводе значений: длина должна выражаться числом!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Rectangle rectangle = new Rectangle(Convert.ToDouble(a[0]), Convert.ToDouble(a[1]));
+                                rectangle.ShowInfo();
+                                break;
+                            }
+                        }
+                }
+            }
+        }
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Введите Triangle для работы с треугольником");
+            Console.WriteLine("Введите Rectangle для работы с прямоугольником");
+            Console.WriteLine("Введите Circle для работы с окружностью");
+            Console.WriteLine("Введите Square для работы с квадратом");
+        }
+    }
+}
---
+++ Shapes.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test228
+{
+    public abstract class Shapes
+    {
+        public string Name { get; set; }
+        public abstract double GetPerimeter();
+        public abstract double GetArea();
+        public abstract void ShowInfo();
+    }
+
+    public class Validator
+    {
+        public bool typeError(List<string> values)
+        {
+            foreach (string value in values)
+            {
+                if (!double.TryParse(value, out double result))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public bool valueError(List<string> values)
+        {
+            if (!typeError(values))
+            {
+                foreach (string value in values)
+                {
+                    if (double.Parse(value) < 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public class Rectangle : Shapes
+    {
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double GetPerimeter() => Width * 2 + Height * 2;
+
+        public override double GetArea() => Width * Height;
+        public double GetDiagonal() => Math.Sqrt(Math.Pow(Height, 2) + Math.Pow(Width, 2));
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Периметр прямоугольника p={GetPerimeter()} ");
+            Console.WriteLine($"Площадь прямоугольника s={GetArea()} ");
+            Console.WriteLine($"Диагональ прямоугольника d={GetDiagonal()} ");
+        }
+    }
+    public class Circle : Shapes
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+        public override double GetPerimeter() => Radius * 2 * Math.PI;
+
+        public override double GetArea() => Radius * Radius * Math.PI;
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Периметр окружности p={GetPerimeter()} ");
+            Console.WriteLine($"Площадь окружности s={GetArea()} ");
+            Console.WriteLine($"Диаметр окружности p={2 * Radius} ");
+        }
+    }
+    public class Square : Shapes
+    {
+        public Square(double length)
+        {
+            this.length = length;
+        }
+
+        public double length;
+        public override double GetArea() => Math.Pow(length, 2);
+
+        public override double GetPerimeter() => length * 4;
+        public double GetDiagonal() => Math.Sqrt(2 * Math.Pow(length, 2));
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Периметр квадрата p={GetPerimeter()} ");
+            Console.WriteLine($"Площадь квадрата s={GetArea()} ");
+            Console.WriteLine($"Диагональ квадрата d={GetDiagonal()} ");
+        }
+    }
+    public class Triangle : Shapes
+    {
+        public Triangle(double side1, double side2, double side3, int side)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+            this.side = side;
+        }
+        double side1, side2, side3;
+        int side;
+        public bool PrivateTriangleValidation()
+        {
+            if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2)
+            { Console.WriteLine("Некорректно введены данные"); return false; }
+            else
+            {
+                return true;
+            }
+        }
+        public override double GetArea()
+        {
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Pow(p * (p - side1) * (p - side2) * (p - side3), 2);
+        }
+        public override double GetPerimeter()
+        {
+            return side1 + side2 + side3;
+        }
+        public double GetHigh(int side)
+        {
+            List<double> sides = new List<double> { side1, side2, side3 };
+            return (2 * GetArea()) / sides[side];
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Периметр треугольника p={GetPerimeter()} ");
+            Console.WriteLine($"Площадь треугольника s={GetArea()} ");
+            Console.WriteLine($"Высота треугольника, проведенная к {side}-ой стороне h={GetHigh(side)}");
+        }
+    }
+}
